58ab72863614c707fd4a07fc677e17ef7a2a0321
Thomas Cuthbert <tcuthbert90@gmail.com>
2015-03-07 20:49:47 +0800

    changed tombomation tcuthbert

--- tcuthbert/wsgi.py
+++ tcuthbert/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tombomation project.
+WSGI config for tcuthbert project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tombomation.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tcuthbert.settings")
 
 from django.core.wsgi import get_wsgi_application
 from dj_static import Cling
